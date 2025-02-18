77f405dba6481ef41acdb90b5404389657737a8a
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-18 18:39:42 +0530

    wrap entire app into ThemeProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import './App.css'
+import "./App.css";
+import { ThemeProvider } from "react";
+import Home from "./pages/Home";
 
 function App() {
-  return ()
+	return (
+		<ThemeProvider>
+			<Home />
+		</ThemeProvider>
+	);
 }
 
-export default App
+export default App;
