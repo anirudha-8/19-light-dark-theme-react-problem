ce74b514397c801dc8722dfa13dac0926c00e62a
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-18 18:46:18 +0530

    create Home page and rename ThemeContext

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,15 @@
+import { useContext } from "react";
+
+import { ThemeContext } from "../context/ThemeContext.jsx";
+
 const Home = () => {
-	return <div>Home</div>;
+	const { theme, toggleTheme } = useContext(ThemeContext);
+
+	return (
+		<div>
+			<h1>{theme.toUpperCase()} MODE</h1>
+			<button onClick={toggleTheme}>Change Theme</button>
+		</div>
+	);
 };
 export default Home;
