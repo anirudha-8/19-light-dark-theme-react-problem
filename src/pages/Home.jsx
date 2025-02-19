dfad15c98c012a3cac36fc4461f05b6eb9f1f2f3
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-19 10:19:33 +0530

    add styling to button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,21 @@ const Home = () => {
 	return (
 		<div>
 			<h1>{theme.toUpperCase()} MODE</h1>
-			<button onClick={toggleTheme}>Change Theme</button>
+			<button
+				onClick={toggleTheme}
+				style={{
+					padding: "10px 20px",
+					fontSize: "16px",
+					cursor: "pointer",
+					border: "none",
+					background: theme === "light" ? "#000" : "#fff",
+					color: theme === "light" ? "#fff" : "#000",
+					borderRadius: "5px",
+					marginTop: "10px",
+				}}
+			>
+				Change Theme
+			</button>
 		</div>
 	);
 };
