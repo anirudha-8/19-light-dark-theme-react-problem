e812c85ca5709d2f51eca5bb1eb5dfdc4cf6a719
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-19 10:12:10 +0530

    wrap app into theme provider

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { ThemeProvider } from "./context/ThemeContext";
+import { Home } from "./pages/Home.jsx";
+
+const App = () => {
+	return (
+		<ThemeProvider>
+			<Home />
+		</ThemeProvider>
+	);
+};
+export default App;
