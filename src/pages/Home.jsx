2ac8c5d86640df426b30b631138773b907be1715
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-19 10:23:39 +0530

    add styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,19 @@ const Home = () => {
 	const { theme, toggleTheme } = useContext(ThemeContext);
 
 	return (
-		<div>
-			<h1>{theme.toUpperCase()} MODE</h1>
+		<div
+			style={{
+				background: theme === "light" ? "#fff" : "#333",
+				color: theme === "light" ? "#000" : "#fff",
+				height: "100vh",
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+				flexDirection: "column",
+				transition: "all 0.3s ease",
+			}}
+		>
+			<h1>{theme === "light" ? "LIGHT" : "DARK"} MODE</h1>
 			<button
 				onClick={toggleTheme}
 				style={{
