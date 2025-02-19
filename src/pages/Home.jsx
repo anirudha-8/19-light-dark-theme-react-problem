bdb26277efa9ba725ef6c671e4dec8ce02ec2af1
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-19 10:17:20 +0530

    convert to uppercase

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
+
+const Home = () => {
+	const { theme, toggleTheme } = useContext(ThemeContext);
+
+	return (
+		<div>
+			<h1>{theme.toUpperCase()} MODE</h1>
+			<button onClick={toggleTheme}>Change Theme</button>
+		</div>
+	);
+};
+export default Home;
